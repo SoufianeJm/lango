78758fade92e8faad673f09fc8366564e2bb5037
Soufiane Joumal <joumalsoufian17@gmail.com>
2025-06-05 23:06:12 +0100

    typography and colors are added

---
+++ lib/core/themes/app_colors.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+
+  static const Color black = Color(0xFF0B0B0B);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color purple = Color(0xFF8000AA); 
+  static const LinearGradient brandGradient = LinearGradient(
+    colors: [
+      Color(0xFFAE0EFF),
+      Color(0xFFFC1D22),
+      Color(0xFFFFA523),
+    ],
+    begin: Alignment.centerLeft,
+    end: Alignment.centerRight,
+  );
+
+
+  static const Color success = Color(0xFF13E800);
+  static const Color warning = Color(0xFFFFB800);
+  static const Color error = Color(0xFFFF3333);
+
+
+  static const Color grey900 = Color(0xFF0C0C0C);
+  static const Color grey600 = Color(0xFF484848);
+  static const Color grey400 = Color(0xFF8D8D8D);
+  static const Color grey100 = Color(0xFFF5F5F5);
+
+
+  static Color withOpacity(Color color, double opacity) {
+    return color.withOpacity(opacity);
+  }
+}
---
+++ lib/core/themes/typography.dart
@@ -0,0 +1,313 @@
+import 'package:flutter/material.dart';
+
+class AppTypography {
+  static const String _fontFamily = 'Poppins';
+
+  static const TextStyle h1Bold = TextStyle(
+    fontSize: 40,
+    fontWeight: FontWeight.w700,
+    fontFamily: _fontFamily,
+    height: 1.3,
+  );
+
+  static const TextStyle h1SemiBold = TextStyle(
+    fontSize: 40,
+    fontWeight: FontWeight.w600,
+    fontFamily: _fontFamily,
+    height: 1.3,
+  );
+
+  static const TextStyle h1Medium = TextStyle(
+    fontSize: 40,
+    fontWeight: FontWeight.w500,
+    fontFamily: _fontFamily,
+    height: 1.3,
+  );
+
+  static const TextStyle h1Regular = TextStyle(
+    fontSize: 40,
+    fontWeight: FontWeight.w400,
+    fontFamily: _fontFamily,
+    height: 1.3,
+  );
+
+  static const TextStyle h2Bold = TextStyle(
+    fontSize: 30,
+    fontWeight: FontWeight.w700,
+    fontFamily: _fontFamily,
+    height: 1.3,
+  );
+
+  static const TextStyle h2SemiBold = TextStyle(
+    fontSize: 30,
+    fontWeight: FontWeight.w600,
+    fontFamily: _fontFamily,
+    height: 1.3,
+  );
+
+  static const TextStyle h2Medium = TextStyle(
+    fontSize: 30,
+    fontWeight: FontWeight.w500,
+    fontFamily: _fontFamily,
+    height: 1.3,
+  );
+
+  static const TextStyle h2Regular = TextStyle(
+    fontSize: 30,
+    fontWeight: FontWeight.w400,
+    fontFamily: _fontFamily,
+    height: 1.3,
+  );
+
+  static const TextStyle h3Bold = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.w700,
+    fontFamily: _fontFamily,
+    height: 1.4,
+  );
+
+  static const TextStyle h3SemiBold = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.w600,
+    fontFamily: _fontFamily,
+    height: 1.4,
+  );
+
+  static const TextStyle h3Medium = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.w500,
+    fontFamily: _fontFamily,
+    height: 1.4,
+  );
+
+  static const TextStyle h3Regular = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.w400,
+    fontFamily: _fontFamily,
+    height: 1.4,
+  );
+
+  static const TextStyle h4Bold = TextStyle(
+    fontSize: 22,
+    fontWeight: FontWeight.w700,
+    fontFamily: _fontFamily,
+    height: 1.45,
+  );
+
+  static const TextStyle h4SemiBold = TextStyle(
+    fontSize: 22,
+    fontWeight: FontWeight.w600,
+    fontFamily: _fontFamily,
+    height: 1.45,
+  );
+
+  static const TextStyle h4Medium = TextStyle(
+    fontSize: 22,
+    fontWeight: FontWeight.w500,
+    fontFamily: _fontFamily,
+    height: 1.45,
+  );
+
+  static const TextStyle h4Regular = TextStyle(
+    fontSize: 22,
+    fontWeight: FontWeight.w400,
+    fontFamily: _fontFamily,
+    height: 1.45,
+  );
+
+  static const TextStyle h5Bold = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.w700,
+    fontFamily: _fontFamily,
+    height: 1.4,
+  );
+
+  static const TextStyle h5SemiBold = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.w600,
+    fontFamily: _fontFamily,
+    height: 1.4,
+  );
+
+  static const TextStyle h5Medium = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.w500,
+    fontFamily: _fontFamily,
+    height: 1.4,
+  );
+
+  static const TextStyle h5Regular = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.w400,
+    fontFamily: _fontFamily,
+    height: 1.4,
+  );
+
+  static const TextStyle h6Bold = TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.w700,
+    fontFamily: _fontFamily,
+    height: 1.45,
+  );
+
+  static const TextStyle h6SemiBold = TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.w600,
+    fontFamily: _fontFamily,
+    height: 1.45,
+  );
+
+  static const TextStyle h6Medium = TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.w500,
+    fontFamily: _fontFamily,
+    height: 1.45,
+  );
+
+  static const TextStyle h6Regular = TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.w400,
+    fontFamily: _fontFamily,
+    height: 1.45,
+  );
+
+  static const TextStyle bodyLargeBold = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w700,
+    fontFamily: _fontFamily,
+    height: 1.4,
+  );
+
+  static const TextStyle bodyLargeSemiBold = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w600,
+    fontFamily: _fontFamily,
+    height: 1.4,
+  );
+
+  static const TextStyle bodyLargeMedium = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w500,
+    fontFamily: _fontFamily,
+    height: 1.4,
+  );
+
+  static const TextStyle bodyLargeRegular = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+    fontFamily: _fontFamily,
+    height: 1.4,
+  );
+
+  static const TextStyle bodyMediumBold = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w700,
+    fontFamily: _fontFamily,
+    height: 1.4,
+  );
+
+  static const TextStyle bodyMediumSemiBold = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w600,
+    fontFamily: _fontFamily,
+    height: 1.4,
+  );
+
+  static const TextStyle bodyMediumMedium = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w500,
+    fontFamily: _fontFamily,
+    height: 1.4,
+  );
+
+  static const TextStyle bodyMediumRegular = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+    fontFamily: _fontFamily,
+    height: 1.4,
+  );
+
+  static const TextStyle bodySmallBold = TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.w700,
+    fontFamily: _fontFamily,
+    height: 1.5,
+  );
+
+  static const TextStyle bodySmallSemiBold = TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.w600,
+    fontFamily: _fontFamily,
+    height: 1.5,
+  );
+
+  static const TextStyle bodySmallMedium = TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.w500,
+    fontFamily: _fontFamily,
+    height: 1.5,
+  );
+
+  static const TextStyle bodySmallRegular = TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.w400,
+    fontFamily: _fontFamily,
+    height: 1.5,
+  );
+
+  static const TextStyle bodyXSmallBold = TextStyle(
+    fontSize: 10,
+    fontWeight: FontWeight.w700,
+    fontFamily: _fontFamily,
+    height: 1.5,
+  );
+
+  static const TextStyle bodyXSmallSemiBold = TextStyle(
+    fontSize: 10,
+    fontWeight: FontWeight.w600,
+    fontFamily: _fontFamily,
+    height: 1.5,
+  );
+
+  static const TextStyle bodyXSmallMedium = TextStyle(
+    fontSize: 10,
+    fontWeight: FontWeight.w500,
+    fontFamily: _fontFamily,
+    height: 1.5,
+  );
+
+  static const TextStyle bodyXSmallRegular = TextStyle(
+    fontSize: 10,
+    fontWeight: FontWeight.w400,
+    fontFamily: _fontFamily,
+    height: 1.5,
+  );
+
+  static const TextStyle bodyXXSmallBold = TextStyle(
+    fontSize: 8,
+    fontWeight: FontWeight.w700,
+    fontFamily: _fontFamily,
+    height: 1.5,
+  );
+
+  static const TextStyle bodyXXSmallSemiBold = TextStyle(
+    fontSize: 8,
+    fontWeight: FontWeight.w600,
+    fontFamily: _fontFamily,
+    height: 1.5,
+  );
+
+  static const TextStyle bodyXXSmallMedium = TextStyle(
+    fontSize: 8,
+    fontWeight: FontWeight.w500,
+    fontFamily: _fontFamily,
+    height: 1.5,
+  );
+
+  static const TextStyle bodyXXSmallRegular = TextStyle(
+    fontSize: 8,
+    fontWeight: FontWeight.w400,
+    fontFamily: _fontFamily,
+    height: 1.5,
+  );
+}
